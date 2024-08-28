3071d9d56f768bb3b5dca229a71ed15f3210f34d
m-heras <mheraspro@gmail.com>
2024-08-28 12:24:21 +0200

    Add ConfigurableSO

---
+++ Core/ConfigurableSO/AConfigurableField.cs
@@ -0,0 +1,27 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using UnityEngine;
+
+namespace MHLib.ConfigurableSO
+{
+    [Serializable]
+    public abstract class AConfigurableField<TConfigurableSO> where TConfigurableSO : AConfigurableSO
+    {
+        [SerializeField, OnValueChanged(nameof(OnConfigurableSOChanged)), BoxGroup("Field", showLabel: false)]
+        protected TConfigurableSO configurableSO;
+        
+        [SerializeReference, HideLabel, HideReferenceObjectPicker, ShowIf(nameof(ShowParameter)), BoxGroup("Field", showLabel: false)]
+        protected object parameter;
+        
+        protected Type parameterType => this.configurableSO != null ? this.configurableSO.ParameterType : null;
+        
+        // We hide the parameter if the configurable SO doesn't have one
+        private bool ShowParameter() => this.parameterType != null;
+        
+        private void OnConfigurableSOChanged()
+        {
+            // Create a new instance of the parameter (so it's not null, and displays properly in the inspector)
+            this.parameter = this.parameterType != null ? Activator.CreateInstance(this.configurableSO.ParameterType) : null;
+        }
+    }
+}
---
+++ Core/ConfigurableSO/AConfigurableSO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace MHLib.ConfigurableSO
+{
+    public abstract class AConfigurableSO : ScriptableObject
+    {
+        // We hardcode the parameter type, set it to null if there's no parameter needed
+        public abstract Type ParameterType { get; }
+    }
+}
