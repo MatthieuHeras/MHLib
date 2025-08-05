3395a6eb69137db800321d119b8cd2be3ed91944
m-heras <mheraspro@gmail.com>
2025-08-05 15:39:14 +0200

    Add Dijkstra algorithm

---
+++ Algorithms/DijkstraNode.cs
@@ -0,0 +1,16 @@
+﻿namespace MHLib
+{
+    public class DijkstraNode<TNode>
+    {
+        public DijkstraNode(TNode node, DijkstraNode<TNode> parent, float distanceFromStart)
+        {
+            this.Node = node;
+            this.Parent = parent;
+            this.DistanceFromStart = distanceFromStart;
+        }
+        
+        public TNode Node { get; private set; }
+        public DijkstraNode<TNode> Parent { get; private set; }
+        public float DistanceFromStart { get; private set; }
+    }
+}
