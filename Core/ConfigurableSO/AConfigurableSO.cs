5fa3910598541fc26ab425cf1f4753f5491540e6
m-heras <mheraspro@gmail.com>
2025-08-07 00:29:03 +0200

    Add summary to ParameterType in AConfigurableSO

--- Core/ConfigurableSO/AConfigurableSO.cs
+++ Core/ConfigurableSO/AConfigurableSO.cs
@@ -5,7 +5,10 @@ namespace MHLib.ConfigurableSO
 {
     public abstract class AConfigurableSO : ScriptableObject
     {
-        // We hardcode the parameter type, set it to null if there's no parameter needed
+        /// <summary>
+        /// We hardcode the parameter type, set it to null if there's no parameter needed.<br/>
+        /// If you need a value type (int, long, etc.), please use the wrappers, as SerializeReference can't support value types.
+        /// </summary>
         public abstract Type ParameterType { get; }
     }
 }
