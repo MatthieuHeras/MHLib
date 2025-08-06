e5a8f922c92c07f069986cf764ab89a137d0f338
m-heras <mheraspro@gmail.com>
2025-08-07 00:29:29 +0200

    Add useful wrapper records for value types for configurable SOs

---
+++ Core/ConfigurableSO/AWrapper.cs
@@ -0,0 +1,20 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using UnityEngine;
+
+namespace MHLib.ConfigurableSO
+{
+    [Serializable]
+    public abstract record AWrapper<T>
+    {
+        [field: SerializeField, HideLabel]
+        public T Value { get; private set; }
+    }
+    
+    [Serializable] public record IntWrapper : AWrapper<int>;
+    [Serializable] public record UintWrapper : AWrapper<uint>;
+    [Serializable] public record LongWrapper : AWrapper<long>;
+    [Serializable] public record UlongWrapper : AWrapper<ulong>;
+    [Serializable] public record FloatWrapper : AWrapper<float>;
+    [Serializable] public record DoubleWrapper : AWrapper<double>;
+}
